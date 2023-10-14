8da878eb3381e9070bf0243f2a49cb3ee0d2a67c
Valeriya Litvinchuk <43747693+vvlit@users.noreply.github.com>
2023-10-14 22:39:35 +0200

    shopping list render

---
+++ src/js/shopping-list.js
@@ -0,0 +1,235 @@
+// const books = [
+//   {
+//         "_id": "642fd89ac8cf5ee957f122b1",
+//         "list_name": "Hardcover Fiction",
+//         "date": "2023-04-07T08:46:57.000Z",
+//         "age_group": "",
+//         "amazon_product_url": "https://www.amazon.com/dp/0593321200?tag=NYTBSREV-20",
+//         "article_chapter_link": "",
+//         "author": "Gabrielle Zevin",
+//         "book_image": "https://storage.googleapis.com/du-prd/books/images/9780593321201.jpg",
+//         "book_image_width": 329,
+//         "book_image_height": 500,
+//         "book_review_link": "",
+//         "book_uri": "nyt://book/bc07e3e3-e917-50bb-9439-70118335f40c",
+//         "contributor": "by Gabrielle Zevin",
+//         "contributor_note": "",
+//         "created_date": "2023-04-05 22:05:23",
+//         "description": "Two friends find their partnership challenged in the world of video game design.",
+//         "first_chapter_link": "",
+//         "price": "0.00",
+//         "primary_isbn10": "0593321200",
+//         "primary_isbn13": "9780593321201",
+//         "publisher": "Knopf",
+//         "rank": 5,
+//         "rank_last_week": 5,
+//         "sunday_review_link": "",
+//         "title": "TOMORROW, AND TOMORROW, AND TOMORROW",
+//         "updated_date": "2023-04-05 22:10:13",
+//         "weeks_on_list": 27,
+//         "buy_links": [
+//           {
+//             "name": "Amazon",
+//             "url": "https://www.amazon.com/dp/0593321200?tag=NYTBSREV-20"
+//           },
+//           {
+//             "name": "Apple Books",
+//             "url": "https://goto.applebooks.apple/9780593321201?at=10lIEQ"
+//           },
+//           {
+//             "name": "Barnes and Noble",
+//             "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9780593321201"
+//           },
+//           {
+//             "name": "Books-A-Million",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FTOMORROW%25252C%252BAND%252BTOMORROW%25252C%252BAND%252BTOMORROW%252FGabrielle%252BZevin%252F9780593321201&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DTOMORROW%25252C%252BAND%252BTOMORROW%25252C%252BAND%252BTOMORROW%252BGabrielle%252BZevin"
+//           },
+//           {
+//             "name": "Bookshop",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9780593321201&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DTOMORROW%252C%2BAND%2BTOMORROW%252C%2BAND%2BTOMORROW"
+//           },
+//           {
+//             "name": "IndieBound",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9780593321201%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DTOMORROW%252C%2BAND%2BTOMORROW%252C%2BAND%2BTOMORROW%2BGabrielle%2BZevin%26aff%3DNYT"
+//           }
+//         ],
+//         "__v": 0
+//       },
+ 
+//       {
+//         "_id": "642fd89ac8cf5ee957f122cc",
+//         "list_name": "Trade Fiction Paperback",
+//         "date": "2023-04-07T08:46:57.000Z",
+//         "age_group": "",
+//         "amazon_product_url": "https://www.amazon.com/Daisy-Jones-Taylor-Jenkins-Reid/dp/1524798622?tag=NYTBSREV-20",
+//         "article_chapter_link": "",
+//         "author": "Taylor Jenkins Reid",
+//         "book_image": "https://storage.googleapis.com/du-prd/books/images/9781524798628.jpg",
+//         "book_image_width": 325,
+//         "book_image_height": 495,
+//         "book_review_link": "https://www.nytimes.com/2019/03/04/books/review/daisy-jones-six-taylor-jenkins-reid.html",
+//         "book_uri": "nyt://book/148f3f21-9a28-583a-bbce-5f7d9f6e65bd",
+//         "contributor": "by Taylor Jenkins Reid",
+//         "contributor_note": "",
+//         "created_date": "2023-04-05 22:05:25",
+//         "description": "A fictional oral history charting the rise and fall of a ’70s rock ’n’ roll band.",
+//         "first_chapter_link": "",
+//         "price": "0.00",
+//         "primary_isbn10": "1524798649",
+//         "primary_isbn13": "9781524798642",
+//         "publisher": "Ballantine",
+//         "rank": 2,
+//         "rank_last_week": 3,
+//         "sunday_review_link": "",
+//         "title": "DAISY JONES & THE SIX",
+//         "updated_date": "2023-04-05 22:10:15",
+//         "weeks_on_list": 20,
+//         "buy_links": [
+//           {
+//             "name": "Amazon",
+//             "url": "https://www.amazon.com/Daisy-Jones-Taylor-Jenkins-Reid/dp/1524798622?tag=NYTBSREV-20"
+//           },
+//           {
+//             "name": "Apple Books",
+//             "url": "https://goto.applebooks.apple/9781524798642?at=10lIEQ"
+//           },
+//           {
+//             "name": "Barnes and Noble",
+//             "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781524798642"
+//           },
+//           {
+//             "name": "Books-A-Million",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FDAISY%252BJONES%252B%252526%252BTHE%252BSIX%252FTaylor%252BJenkins%252BReid%252F9781524798642&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DDAISY%252BJONES%252B%252526%252BTHE%252BSIX%252BTaylor%252BJenkins%252BReid"
+//           },
+//           {
+//             "name": "Bookshop",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781524798642&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DDAISY%2BJONES%2B%2526%2BTHE%2BSIX"
+//           },
+//           {
+//             "name": "IndieBound",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781524798642%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DDAISY%2BJONES%2B%2526%2BTHE%2BSIX%2BTaylor%2BJenkins%2BReid%26aff%3DNYT"
+//           }
+//         ],
+//         "__v": 0
+//       },
+//       {
+//         "_id": "642fd89ac8cf5ee957f122cb",
+//         "list_name": "Trade Fiction Paperback",
+//         "date": "2023-04-07T08:46:57.000Z",
+//         "age_group": "",
+//         "amazon_product_url": "https://www.amazon.com/dp/1668001225?tag=NYTBSREV-20",
+//         "article_chapter_link": "",
+//         "author": "Colleen Hoover",
+//         "book_image": "https://storage.googleapis.com/du-prd/books/images/9781668001226.jpg",
+//         "book_image_width": 322,
+//         "book_image_height": 500,
+//         "book_review_link": "",
+//         "book_uri": "nyt://book/3aa85e47-4df9-53ef-9957-a77753d3502c",
+//         "contributor": "by Colleen Hoover",
+//         "contributor_note": "",
+//         "created_date": "2023-04-05 22:05:25",
+//         "description": "In the sequel to “It Ends With Us,” Lily deals with her jealous ex-husband as she reconnects with her first boyfriend.",
+//         "first_chapter_link": "",
+//         "price": "0.00",
+//         "primary_isbn10": "1668001225",
+//         "primary_isbn13": "9781668001226",
+//         "publisher": "Atria",
+//         "rank": 1,
+//         "rank_last_week": 1,
+//         "sunday_review_link": "",
+//         "title": "IT STARTS WITH US",
+//         "updated_date": "2023-04-05 22:10:15",
+//         "weeks_on_list": 24,
+//         "buy_links": [
+//           {
+//             "name": "Amazon",
+//             "url": "https://www.amazon.com/dp/1668001225?tag=NYTBSREV-20"
+//           },
+//           {
+//             "name": "Apple Books",
+//             "url": "https://goto.applebooks.apple/9781668001226?at=10lIEQ"
+//           },
+//           {
+//             "name": "Barnes and Noble",
+//             "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781668001226"
+//           },
+//           {
+//             "name": "Books-A-Million",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FIT%252BSTARTS%252BWITH%252BUS%252FColleen%252BHoover%252F9781668001226&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DIT%252BSTARTS%252BWITH%252BUS%252BColleen%252BHoover"
+//           },
+//           {
+//             "name": "Bookshop",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781668001226&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DIT%2BSTARTS%2BWITH%2BUS"
+//           },
+//           {
+//             "name": "IndieBound",
+//             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781668001226%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DIT%2BSTARTS%2BWITH%2BUS%2BColleen%2BHoover%26aff%3DNYT"
+//           }
+//         ],
+//         "__v": 0
+//       }
+// ]
+ 
+
+// localStorage.setItem('booksShoppingList', JSON.stringify(books));
+
+const refs = {
+  booksContainerShop: document.querySelector('.books-container-shop'),
+};
+
+function getBooks(key) {
+  try {
+    const savedBooks = localStorage.getItem(key);
+    return JSON.parse(savedBooks);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function createMarkup(books) {
+  const markup = books
+    .map(
+      ({
+        img,
+        title,
+        category,
+        description,
+        author,
+        amazon,
+        apple
+      }) => {
+        return `<div class="book-container">
+        <img src="${img}" alt="${title}" loading="lazy" />
+    <p class="title">${title}</p>
+    <p class="list-name">${category}</p>
+    <p class="description">${description}</p>
+    <p class="author">${author}</p>
+    <ul class="links">
+      <li>
+        <a
+          href="${amazon}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >amazon</a
+        >
+      </li>
+      <li>
+        <a
+          href="${apple}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >apple books</a
+        >
+      </li>
+    </ul>
+    <button class="delete-btn" type="button">delete</button>
+    </div>
+    `;
+      }
+    )
+    .join('');
+
+  refs.booksContainerShop.insertAdjacentHTML('beforeend', markup);
+}
+
+createMarkup(getBooks('booksShoppingList'));
