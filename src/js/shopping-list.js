1975c38f348ce61428559f07c717649158a9b93e
Valeriya Litvinchuk <43747693+vvlit@users.noreply.github.com>
2023-10-15 19:43:02 +0200

    css-mobile

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -15,33 +15,40 @@ function getBooks(key) {
 function createMarkup(books) {
   const markup = books
     .map(({ img, title, category, description, author, amazon, apple }) => {
-      return `<div class="book-container">
-        <img class="img-shop-list" src="${img}" alt="${title}" loading="lazy" />
-    <p class="title-shop-list">${title}</p>
-    <p class="list-name-shop-list">${category}</p>
-    <p class="description-shop-list">${description}</p>
-    <p class="author-shop-list">${author}</p>
-    <ul class="links-shop-list">
-      <li>
-        <a
-          href="${amazon}"
-          target="_blank"
-          rel="noopener noreferrer"
-          >amazon</a
-        >
-      </li>
-      <li>
-        <a
-          href="${apple}"
-          target="_blank"
-          rel="noopener noreferrer"
-          >apple books</a
-        >
-      </li>
-    </ul>
-    <button class="delete-btn-shop-list" type="button">delete</button>
-    </div>
-    `;
+      return `
+        <li class="book-card-shop-list">
+          <img class="img-shop-list" src="${img}" alt="${title}" loading="lazy" />
+          <div class="right-card-part">
+            <div class="card-top-shop-list">
+              <p class="title-shop-list">${title}</p>
+              <p class="category-shop-list">${category}</p>
+              <p class="description-shop-list">${description}</p>
+            </div>
+            <div class="card-bottom-shop-list">
+              <p class="author-shop-list">${author}</p>
+              <ul class="links-shop-list">
+                <li>
+                  <a
+                    href="${amazon}"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >amaz</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="${apple}"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >app</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+          <button class="delete-btn-shop-list" type="button"></button>
+        </li>
+      `;
     })
     .join('');
 
